04d9d3435547780a64706aa36194b28cb874ec49
QuocLamm <quoclam4a@gmail.com>
2024-06-08 18:35:28 +0700

    thêm thực đơn

---
+++ src/components/Menu.js
@@ -0,0 +1,234 @@
+import React from "react";
+import "./Menu.css";
+
+const Menu = () => {
+  return(
+    <div className="product">
+  <div className="container">
+    <div className="w-100">
+      <div className="select-menu">
+      <ul id="list-example">
+        <div class="catItem" href="#Đồ-Uống-Mới-Mùa-Hè">Đồ Uống Mới Mùa Hè</div>
+        <div class="catItem" href="#Đặc-Sản-Của-Cộng">Đặc Sản Của Cộng</div>
+        <div class="catItem" href="#Cà-Phê-Phở">Cà Phê Phở</div>
+        <div class="catItem" href="#Cà-Phê-Ta">Cà Phê Ta</div>
+        <div class="catItem" href="#Trà-Cổ-Thụ">Trà Cổ Thụ</div>
+        <div class="catItem" href="#Đồ-Địa-Phương">Đồ Địa Phương</div>
+        <div class="catItem" href="#Trái-Cây">Trái Cây</div>
+        <div class="catItem" href="#Sữa-Chua">Sữa Chua</div>
+        <div class="catItem" href="#Topping">Topping</div>
+        <div class="catItem" href="#Đồ-Ăn-Chơi">Đồ Ăn Chơi</div>
+        </ul>
+      </div>
+      <div className="shop-product">
+        <div className="list-product text" data-bs-spy="scroll" data-bs-target="#list-example" data-bs-smooth-scroll="true">
+          <div id="Đồ-Uống-Mới-Mùa-Hè">
+          <div class="block_menu_item_title"><h3>Đồ Uống Mới Mùa Hè</h3></div>
+            <div className="row">
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2024/04/Tr%C3%A0-g%E1%BB%ABng.png" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">TRÀ GỪNG</h2>
+                  <span className="price">39,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2024/04/Tr%C3%A0-c%C3%BAc.png" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">TRÀ CÚC</h2>
+                  <span className="price">39,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2024/04/Tr%C3%A0-nh%C3%A0i.png" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">TRÀ NHÀI</h2>
+                  <span className="price">39,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2024/04/Phe%CC%82-cam.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">PHÊ CAM</h2>
+                  <span className="price">39,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2024/04/Phe%CC%82-chanh.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">PHÊ CHANH</h2>
+                  <span className="price">39,000 Đ</span>
+                </div>
+              </div>  
+            </div>
+          </div>
+          <div id="Đặc-Sản-Của-Cộng">
+          <div class="block_menu_item_title"><h3>Đặc Sản Của Cộng</h3></div>
+            <div className="row">
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/02/co%CC%82%CC%81t-du%CC%9B%CC%80a-ca%CC%80-phe%CC%82_lo%CC%9B%CC%81n.png" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">CỐT DỪA CÀ PHÊ</h2>
+                  <span className="price">55,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/02/co%CC%82%CC%81t-du%CC%9B%CC%80a-co%CC%82%CC%81m-xanh.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">CỐT DỪA CỐM XANH</h2>
+                  <span className="price">65,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/02/co%CC%82%CC%81t-du%CC%9B%CC%80a-%C4%91a%CC%A3%CC%82u-xanh_1.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">CỐT DỪA ĐẬU XANH</h2>
+                  <span className="price">65,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/02/co%CC%82%CC%81t-du%CC%9B%CC%80a-cacao.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">CỐT DỪA CA CAO</h2>
+                  <span className="price">65,000 Đ</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="Cà-Phê-Phở">
+          <div class="block_menu_item_title"><h3>Cà Phê Phở</h3></div>
+            <div className="row">
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/10/c%C3%A0-ph%C3%AA-ph%E1%BB%9F-k%C3%A8m-kem-resize.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">CÀ PHÊ PHỞ KÈM KEM</h2>
+                  <span className="price">45,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/12/Ba%CC%A3c-xi%CC%89u-pho%CC%9B%CC%89-%C4%91a%CC%81_600x600px.png" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">BẠC XỈU PHỞ</h2>
+                  <span className="price">45,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/12/Na%CC%82u-pho%CC%9B%CC%89-%C4%91a%CC%81_600x600px-1.png" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">NÂU PHỞ</h2>
+                  <span className="price">35,000 Đ</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="Cà-Phê-Ta">
+          <div class="block_menu_item_title"><h3>Cà Phê Ta</h3></div>
+            <div className="row">
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/12/Na%CC%82u-kem_600x600px.png" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">NÂU KEM</h2>
+                  <span className="price">45,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/12/Ba%CC%A3c-xi%CC%89u-%C4%91a%CC%81-size-lo%CC%9B%CC%81n.png" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">BẠC XỈU</h2>
+                  <span className="price">45,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/02/su%CC%9B%CC%83a-%C4%91a%CC%81-sa%CC%80i-go%CC%80n_lo%CC%9B%CC%81n.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">SỮA ĐÁ SÀI GÒN</h2>
+                  <span className="price">41,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/02/vina-latte.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">VINA-LATTE</h2>
+                  <span className="price">45,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/02/vina-cappu.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">VINA-CAPPUCHINO</h2>
+                  <span className="price">41,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/02/vinacano-%C4%91a%CC%81.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">VINA-CANO</h2>
+                  <span className="price">35,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/02/%C4%91en-%C4%91a%CC%81_lo%CC%9B%CC%81n.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">CÀ PHÊ ĐEN</h2>
+                  <span className="price">29,000 Đ</span>
+                </div>
+              </div>
+              <div className="col-6 col-sm-4">
+                <div className="itemProducts">
+                  <div className="img-product">
+                    <img src="https://cong-news.appwifi.com/wp-content/uploads/2023/02/na%CC%82u-%C4%91a%CC%81_lo%CC%9B%CC%81n.jpg" alt=""/>
+                  </div>
+                  <h2 className="itemTitle">CÀ PHÊ NÂU</h2>
+                  <span className="price">35,000 Đ</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  );
+};
+
+export default Menu;
